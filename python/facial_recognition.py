6e6111147bfcc0ec94ff8cffd0208e00de496a1d
Christopher Wood <cmw2379@gmail.com>
2017-01-21 00:15:07 -0500

    Basic working facial tracking in python

--- python/facial_recognition.py
+++ python/facial_recognition.py
@@ -3,8 +3,8 @@ import cv2
 import time
 
 frames_per_second = 20
-frame_length = frames_per_second/1000
-current_milliseconds = lambda: int(round(time.time() * 1000))
+frame_length = 1000.0/frames_per_second
+current_milliseconds = lambda: int(round(time.time() * 1000.0))
 
 cascPath = "../haarcascades/haarcascade_frontalface_default.xml"
 faceCascade = cv2.CascadeClassifier(cascPath)
@@ -25,38 +25,20 @@ while(True):
 	)
 
 	for (x, y, w, h) in faces:
+		cv2.putText(videoFrame, "Unknown", (x, y - 5), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0))
 		cv2.rectangle(videoFrame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
 	cv2.imshow('Live Feed', videoFrame)
 
 	execution_time = current_milliseconds() - execution_start
+	print(execution_time)
 
 	if execution_time < frame_length:
 		remaining_frame_time = frame_length - execution_time
-		sleep(remaining_frame_time/1000)
+		time.sleep(remaining_frame_time/1000)
 
-#imagePath = sys.argv[1]
-#cascPath = "../haarcascades/haarcascade_frontalface_default.xml"
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+	   break
 
-#faceCascade = cv2.CascadeClassifier(cascPath)
-
-#image = cv2.imread(imagePath)
-#gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-#
-#faces = faceCascade.detectMultiScale(
-#    gray,
-#    scaleFactor=1.1,
-#    minNeighbors=5,
-#    minSize=(30, 30),
-#    flags = cv2.CASCADE_SCALE_IMAGE
-#)
-
-#print "Found {0} faces!".format(len(faces))
-
-# Draw a rectangle around the faces
-#for (x, y, w, h) in faces:
-#    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-#cv2.imshow("Faces found" ,image)
 cap.release()
 cv2.waitKey(0)
