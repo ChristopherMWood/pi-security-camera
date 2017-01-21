333c2ab7c6782b0f6d4c389417b03bbbd7ce12c5
Christopher Wood <cmw2379@gmail.com>
2017-01-20 23:03:13 -0500

    Beginning to video program

--- python/facial_recognition.py
+++ python/facial_recognition.py
@@ -1,27 +1,70 @@
 import sys
 import cv2
+import time
 
-imagePath = sys.argv[1]
+frames_per_second = 5
+frame_length = frames_per_second/1000
+
+current_milliseconds = lambda: int(round(time.time() * 1000))
+
 cascPath = "../haarcascades/haarcascade_frontalface_default.xml"
-
 faceCascade = cv2.CascadeClassifier(cascPath)
 
-image = cv2.imread(imagePath)
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+videoStream = cv2.VideoCapture(0)
 
-faces = faceCascade.detectMultiScale(
-    gray,
-    scaleFactor=1.1,
-    minNeighbors=5,
-    minSize=(30, 30),
-    flags = cv2.CASCADE_SCALE_IMAGE
-)
+time_difference = 0
 
-print "Found {0} faces!".format(len(faces))
+while(True):
+	execution_start = current_milliseconds
+
+	ret, videoFrame = videoStream.read()
+
+	gray = cv2.cvtColor(videoFrame, cv2.COLOR_BGR2GRAY)
+
+	faces = faceCascade.detectMultiScale(
+		gray,
+		scaleFactor = 1.1,
+		minNeighbors = 5,
+		minSize = (30, 30),
+		flags = cv2.CASCADE_SCALE_IMAGE
+	)
+
+	for (x, y, w, h) in faces:
+		cv2.rectangle(videoFrame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+	cv2.imshow('frame', videoFrame)
+
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+
+	execution_time = current_milliseconds - execution_start
+
+	if execution_time < frame_length:
+		remaining_frame_time = frame_length - execution_time
+		sleep(remaining_frame_time/1000)
+
+#imagePath = sys.argv[1]
+#cascPath = "../haarcascades/haarcascade_frontalface_default.xml"
+
+#faceCascade = cv2.CascadeClassifier(cascPath)
+
+#image = cv2.imread(imagePath)
+#gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+#
+#faces = faceCascade.detectMultiScale(
+#    gray,
+#    scaleFactor=1.1,
+#    minNeighbors=5,
+#    minSize=(30, 30),
+#    flags = cv2.CASCADE_SCALE_IMAGE
+#)
+
+#print "Found {0} faces!".format(len(faces))
 
 # Draw a rectangle around the faces
-for (x, y, w, h) in faces:
-    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+#for (x, y, w, h) in faces:
+#    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-cv2.imshow("Faces found" ,image)
+#cv2.imshow("Faces found" ,image)
+cap.release()
 cv2.waitKey(0)
