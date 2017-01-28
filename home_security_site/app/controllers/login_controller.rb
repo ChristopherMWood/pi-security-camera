bf2aaff5cabb9cf6bc47ea153699e3ecc4d93d21
Christopher Wood <cmw2379@gmail.com>
2017-01-28 12:08:51 -0500

    organized stylesheets for website

--- home_security_site/app/controllers/login_controller.rb
+++ home_security_site/app/controllers/login_controller.rb
@@ -1,4 +1,4 @@
-class LoginController < ActionController::Base
+class LoginController < ApplicationController
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
